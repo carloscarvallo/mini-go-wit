b2cc6ad584dfccdc1e6b972271d5bcfd60acc6ef
carloscarvallo <carloscarvallo87@gmail.com>
2016-12-13 17:58:54 -0300

    comments

--- main.go
+++ main.go
@@ -11,31 +11,38 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const (
+	baseURL = "https://api.wit.ai/message"
+)
+
 func main() {
+	// Read environment variables
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	wittoken := os.Getenv("WITAI_TOKEN")
 
+	// Hardcoding message yet
 	msg := "Cual es el clima en Asuncion?"
+
+	// Attaching query params for the wit.ai API
 	v := url.Values{}
 	v.Add("v", "2016052")
 	v.Add("q", msg)
+	// Encode all values
 	encodedValues := v.Encode()
-	baseURL := "https://api.wit.ai/message"
 
+	// Return a string with that format
 	url := fmt.Sprintf("%s?%s", baseURL, encodedValues)
+	// Use NewRequest for control HTTP headers in this case adding wit_token
 	req, _ := http.NewRequest("GET", url, nil)
-
 	req.Header.Add("authorization", "Bearer "+wittoken)
-
 	res, _ := http.DefaultClient.Do(req)
 
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
-	fmt.Println(res)
+	// printing the body
 	fmt.Println(string(body))
 }
