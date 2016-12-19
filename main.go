f2bafadd52a8d60d52aa39458eb9261437d3f9ab
carloscarvallo <carloscarvallo87@gmail.com>
2016-12-19 18:31:45 -0300

    setting route for messenger api

--- main.go
+++ main.go
@@ -13,17 +13,27 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var err = godotenv.Load()
+
+var wit_token = os.Getenv("WIT_TOKEN")
+var fb_token = os.Getenv("FB_TOKEN")
+var app_token = os.Getenv("APP_TOKEN")
+
 const (
 	baseURL = "https://api.wit.ai"
 )
-
-var err = godotenv.Load()
-var wittoken = os.Getenv("WITAI_TOKEN")
 
 // Message struct for json
 type Message struct {
 	ID      int    `json:"id,omitempty"`
 	Message string `json:"message,omitempty"`
+}
+
+func tokenVerify(w http.ResponseWriter, req *http.Request) {
+	params := req.URL.Query()
+	hub_mode := params.Get("hub.mode")
+	verify_token := params.Get("hub.verify_token")
+	fmt.Println(hub_mode, verify_token)
 }
 
 func postMessage(w http.ResponseWriter, req *http.Request) {
@@ -33,27 +43,29 @@ func postMessage(w http.ResponseWriter, req *http.Request) {
 	if decErr != nil {
 		log.Fatal(decErr)
 	}
-	
+
 	// adding uri resource
 	resource := "/message"
 	u, _ := url.ParseRequestURI(baseURL)
 	u.Path = resource
-	
+
 	// attaching query params
 	v := url.Values{}
 	v.Add("v", "2016052")
 	v.Add("q", msg.Message)
 	encodedValues := v.Encode()
 	url := fmt.Sprintf("%s?%s", u, encodedValues)
-	
+
 	// make request
 	request, _ := http.NewRequest("GET", url, nil)
-	request.Header.Add("authorization", "Bearer "+wittoken)
-	
+	request.Header.Add("authorization", "Bearer "+wit_token)
+
 	res, _ := http.DefaultClient.Do(request)
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-	
+
+	fmt.Println(string(body))
+
 	// write json to http.Response
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(body)
@@ -62,5 +74,6 @@ func postMessage(w http.ResponseWriter, req *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/message", postMessage).Methods("POST")
-	log.Fatal(http.ListenAndServe(":12345", router))
+	router.HandleFunc("/webhook", tokenVerify).Methods("GET")
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
